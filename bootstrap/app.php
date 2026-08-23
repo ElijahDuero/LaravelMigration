0536ac63e6231ce5f19acfc7d2c73cb25f203962
ElijahDuero <elijahduero42@gmail.com>
2026-08-23 22:51:56 +0800

    debug: concise crash logging

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -34,7 +34,7 @@ $app = Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->report(function (\Throwable $e) {
-            error_log('[LARAVEL EXCEPTION] ' . get_class($e) . ': ' . $e->getMessage() . "\n" . $e->getFile() . ':' . $e->getLine() . "\n" . $e->getTraceAsString());
+            error_log('!!! CRASH: ' . get_class($e) . ' => ' . $e->getMessage() . ' IN ' . $e->getFile() . ':' . $e->getLine());
         });
 
         $exceptions->shouldRenderJsonWhen(
