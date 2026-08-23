2f5cd3774ebbc6b2a00c7c6d19af03a2dffa83cc
ElijahDuero <elijahduero42@gmail.com>
2026-08-23 22:38:48 +0800

    debug: force PHP error display in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,10 @@
 <?php
 
+// Temporary: force PHP error display to catch silent 500s
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 $tmpDirs = [
     '/tmp/storage/app/public',
     '/tmp/storage/framework/cache/data',
